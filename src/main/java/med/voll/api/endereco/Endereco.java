31397624816f4143d29ce0db4e81ed84d78b98d0
Artur Alexandre <97802476+arturalexandree@users.noreply.github.com>
2023-10-07 09:31:55 -0300

    medicorepository

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -12,6 +12,15 @@ import med.voll.api.medico.Especialidade;
 @AllArgsConstructor
 public class Endereco {
 
+	 public Endereco(DadosEndereco dados) {
+	        this.logradouro = dados.logradouro();
+	        this.bairro = dados.bairro();
+	        this.cep = dados.cep();
+	        this.uf = dados.uf();
+	        this.cidade = dados.cidade();
+	        this.numero = dados.numero();
+	        this.complemento = dados.complemento();
+	    }
 	private String logradouro;
     private String bairro;
     private String cep;
