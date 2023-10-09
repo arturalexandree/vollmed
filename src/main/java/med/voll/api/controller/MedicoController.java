96694b4f3ca2213dda0056efff368442ab1ce765
Artur Alexandre <97802476+arturalexandree@users.noreply.github.com>
2023-10-09 14:57:54 -0300

    adicionando atualizacao de

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,12 +9,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -38,4 +40,12 @@ public class MedicoController {
 		        return repository.findAll(paginacao).map(DadosListagemMedico::new);
 
 		   }
+		   
+		   
+		    @PutMapping
+		    @Transactional
+		    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		    	var medico = repository.getById(dados.id());
+		    	medico.atualizarInformacoes(dados);
+		    }
 	   }
