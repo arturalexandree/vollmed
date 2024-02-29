051fd0923a69b6bbf24ed680569ffebad260f6c5
arturalexandree <arturalexandredemelo@gmail.com>
2024-02-29 17:07:23 -0300

    Delete de medico implementado

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -26,8 +26,10 @@ public class Medico {
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -49,4 +51,8 @@ public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
         this.endereco.atualizarInformacoes(dados.endereco());
     }
 }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
